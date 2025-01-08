3020022df7822a3b362548f2714c5959966063ce
Luke Hannan <luke@lukehannan.com>
2025-01-08 11:43:23 +0200

    Update functions to speed up performance

--- R/nparACT_auxfunctions1.R
+++ R/nparACT_auxfunctions1.R
@@ -1,72 +1,82 @@
-nparACT_auxfunctions1 <- list(
-  nparACT_data_hrs = function(data, a, m){
-    data_hrs <- matrix(NA, nrow = a/m) 
-    for (i in 1:(a/m)){
-      subset_h <- data$activity[(((i-1)*m)+1):((i*m))]
-      mean_subset_h <- mean(subset_h)
-      data_hrs[i] <- mean_subset_h 
-    }
-    return(data_hrs)
-  },
-  
-  nparACT_data_min = function(b, SR, data){
-    data_min <- matrix(NA, b) 
-    for (d in 1:b){ 
-      subset_min <- data$activity[(((d-1)*(SR*60))+1):((d*(SR*60)))]
-      data_min[d] <- mean(subset_min)
-    }
-    return(data_min)
-  },
-  
-  nparACT_filt = function(data, a, cutoff){
-    for (k in 1:a){
-      if (data$activity[k] < cutoff){
-        data$activity[k] <- 0
-      }
-    }
-  },
-  
-  nparACT_minaverage = function(b, data_min){
-    ## ---- Minutewise averages across 24hrs -> 1440 values
-    c <- ceiling(b/1440)  
-    data_min[c*1440] <- NA 
-    minaverage <- matrix(NA, 1440)
-    for (i in 1:1440){    
-      for_minaverage <- data_min[c(seq(i,c*1440,1440))]
-      minaverage[i] <- mean(for_minaverage, na.rm = T)
-    }
-    return(minaverage)
-  },
-  
+nparACT_data_hrs = function(data, a, m) {
+  data_hrs = matrix(
+    vapply(seq_len((a / m)), function(x) {
+      mean.default(.subset2(data, "activity")[(((x - 1) * m) + 1):((x * m))])
+    }, numeric(1)),
+    ncol = 1
+  )
+
+  return(data_hrs)
+}
+
+nparACT_data_min = function(b, SR, data) {
+  data_min = matrix(
+    vapply(seq_len(b), function(x) {
+      mean.default(
+        .subset2(data, "activity")[(((x - 1) * (SR * 60)) + 1):((x * (SR * 60)))]
+      )
+    }, numeric(1)),
+    ncol = 1
+  )
+
+  return(data_min)
+}
+
+nparACT_filt = function(data, cutoff) {
+  ifelse(.subset2(data, "activity") < cutoff, 0, .subset2(data, "activity"))
+}
+
+nparACT_minaverage = function(b, data_min) {
+  ## ---- Minutewise averages across 24hrs -> 1440 values
+  c = ceiling(b / 1440)
+  data_min[c * 1440] = NA
+
+  minaverage = matrix(
+    vapply(seq_len(1440), function(x) {
+      mean.default(data_min[c(seq(x, c * 1440, 1440))], na.rm = TRUE)
+    }, numeric(1)),
+    nrow = 1440, ncol = 1
+  )
+
+  return(minaverage)
+}
+
 ## ---- Hourly averages
-  nparACT_hraverage_GA_loop = function(minaverage, data, a , SR){
-    hraverage <- matrix(NA)
-    for (i in 1:24){
-      hraverage [i] <- mean(minaverage[(((i-1)*60)+1):(60*i)])
-    }
-    daytime <- matrix(NA)
-    time <- data$time
-    time <- as.character(time)
-    for (v in seq(1,a,(SR*60*60))){  
-      daytime[v] <- time[v]
-    }
-    daytime <- na.omit(daytime)
-    daytime <- as.character(daytime)
-    temp = unlist(str_split(daytime, ' ') )
-    temp_nums = 1:length(temp)
-    timeinfo = temp[ (temp_nums %% 2) == 0 ] 
-    temp = unlist(str_split(timeinfo, ':') )
-    temp_nums = 1:length(temp)
-    timeinfo = temp[ (temp_nums %% 3) == 1 ] 
-    start.time <- as.numeric(timeinfo[1])  
-    for_hraverage_time <- rep(seq(1,24),2)
-    seq <- seq(start.time, length.out = 24)
-    hraverage_time <- for_hraverage_time[seq]
-    hraverage_time <- as.numeric(hraverage_time)
-    hraverage_time[hraverage_time==24] <- 0
-    df_hraverage <- data.frame(hraverage_time, hraverage)
-    df_hraverage <- df_hraverage[ order(hraverage_time, hraverage), ]
-    hraverage_sorted <- df_hraverage[, 2]
-    return(hraverage_sorted)
+nparACT_hraverage_GA_loop = function(minaverage, data, a, SR) {
+
+  hraverage = matrix(
+    vapply(seq_len(24), function(x) {
+      mean.default(minaverage[(((x - 1) * 60) + 1):(60 * x)])
+    }),
+    nrow = 24, ncol = 1
+  )
+
+  daytime <- matrix(NA)
+  time <- data[["time"]]
+  time <- as.character(time)
+
+  for (v in seq(1, a, (SR * 60 * 60))) {
+    daytime[v] <- time[v]
   }
-)
+
+  daytime <- stats::na.omit(daytime)
+  daytime <- as.character(daytime)
+
+  temp = unlist(stringr::str_split(daytime, " "))
+  temp_nums = seq_along(temp)
+  timeinfo = temp[(temp_nums %% 2) == 0]
+  temp = unlist(stringr::str_split(timeinfo, ":"))
+  temp_nums = seq_along(temp)
+  timeinfo = temp[(temp_nums %% 3) == 1]
+
+  start.time <- as.numeric(timeinfo[1])
+  for_hraverage_time <- rep(seq(1, 24), 2)
+  seq <- seq(start.time, length.out = 24)
+  hraverage_time <- for_hraverage_time[seq]
+  hraverage_time <- as.numeric(hraverage_time)
+  hraverage_time[hraverage_time == 24] <- 0
+  df_hraverage <- data.frame(hraverage_time, hraverage)
+  df_hraverage <- df_hraverage[order(hraverage_time, hraverage), ]
+
+  return(df_hraverage[, 2])
+}
--- R/nparACT_flex.R
+++ R/nparACT_flex.R
@@ -1,87 +1,123 @@
-nparACT_flex <-
-function (name, SR, cutoff = 1, minutes, plot = T, fulldays = T){
-  data <- get(name)
-  if (is.data.frame(data)==F){
+#' nparACT_flex
+#'
+#' Function returns IS, IV, RA, L5, M10 as well as L5 and M10 start times for single actigraphy files.
+#' Additionally, it also returns the L value (Lflex) and the start time for a specified number of minutes.
+#' The function also returns a classic dual day plot for the data, a plot of minutewise averages across
+#' 24 hours as well as a plot for hourly averages across 24 hours.
+#'
+#' @param data Data.frame containing the actigraphy data. Actigraphy files must contain
+#' a first column with a date/time vector, which must be of form YYYY-MM-DD
+#' HH:MM:SS or YYYY/MM/DD HH:MM:SS, i.e. in an unambiguous date format and a
+#' second column with actigraphy values.
+#' @param SR Sampling rate in Hz.
+#' @param cutoff Can be used to define a cutoff for actigraphy data. Default value is 1, i.e. all
+#' values are taken into account.
+#' @param minutes Number of minutes for which the Lflex value should be computed.
+#' @param plot If TRUE plots are produced. Default is TRUE.
+#' @param fulldays If TRUE only data from multiples of 24 hours (i.e. full days) are included while
+#' the rest of the data are discarded. Default is TRUE.
+#' @param quiet If TRUE will supress all warnings.
+#' @return Data frame containing IS, IV and RA values. Besides this, L5, M10 and Lflex (for the number of
+#' minutes specified) values are given along with the respective start time.
+#' @examples
+#' # example function nparACT
+#' data(sleepstudy)
+#' r <- nparACT_flex(sleepstudy, SR = 4/60, minutes = 435)
+#' @export
+nparACT_flex <- function(data, SR, cutoff = 1, minutes, plot = FALSE, fulldays = TRUE, quiet = FALSE) {
+
+  if (is.data.frame(data) == FALSE) {
     data = as.data.frame(data)
   }
-  if(ncol(data) == 2){
-    data[,1] <- as.POSIXct(data[,1])
-    data[,2] <- as.numeric(as.character(data[,2]))
-    names(data)[1] <- "time"
-    names(data)[2] <- "activity"
-  } 
-  if(ncol(data) == 3){
-    names(data)[1] <- "date"
-    names(data)[2] <- "time"
-    names(data)[3] <- "activity"
-    data$date <- NULL
-    data$time <- as.POSIXct(data$time, format="%H:%M:%S")  
-    data$activity <- as.numeric(as.character(data$activity))
+
+  if (ncol(data) == 2) {
+    if (!inherits(data[[1]], c("Date", "POSIXt"))) {
+      data[[1]] <- lubridate::parse_date_time(data[[1]], c("y-m-d H:M:S", "y-m-d"), quiet = quiet)
+    }
+
+    if (!is.numeric(data[[2]])) {
+      data[[2]] <- as.numeric(as.character(data[[2]]))
+    }
+
+    data.table::setnames(data, new = c("time", "activity"))
   }
+
+  if (ncol(data) == 3) {
+    if (!inherits(data[[2]], c("Date", "POSIXt"))) {
+      data[[2]] <- lubridate::parse_date_time(data[[2]], "H:M:S", quiet = quiet)
+    }
+
+    if (!is.numeric(data[[3]])) {
+      data[[3]] <- as.numeric(as.character(data[[3]]))
+    }
+
+    data.table::setnames(data, new = c("date", "time", "activity"))
+    data[["date"]] <- NULL
+  }
+
   if (any(is.na(data$activity)) == TRUE) stop("Please check your data! It must not contain NAs")
-  
-  bin_hr <- 60  
-  a <- nrow(data) 
-  e <- SR*60 ## samples per minute
-  m <- bin_hr*SR*60  ## samples per hour
-  full_days <- floor(a/(e*bin_hr*24))
-  
+
+  bin_hr <- 60
+  a <- nrow(data)
+  e <- SR * 60 ## samples per minute
+  m <- bin_hr * SR * 60 ## samples per hour
+  full_days <- floor(a / (e * bin_hr * 24))
+
   ## --- Cut data to full days
-  if (fulldays == T){
-    data <- data[1:(e*bin_hr*24*full_days),]
+  if (fulldays == TRUE) {
+    data <- data[1:(e * bin_hr * 24 * full_days), ]
   }
-  a <- nrow(data) 
-  b <- floor(a/(SR*60)) ## full minutes recorded
+  a <- nrow(data)
+  b <- floor(a / (SR * 60)) ## full minutes recorded
   ## ------------------------------------------
-  
+
   ## ---- Filtering, Cutoff for classification as movement
-  nparACT_auxfunctions1$nparACT_filt(data, a, cutoff)
+  data[["activity"]] = nparACT_filt(data, cutoff)
   ## ------------------------------------------
-  
+
   ## ---- Calculate average for each minute (needed if SR != 1/60)
-  if (SR != 1/60){
-    data_min <- nparACT_auxfunctions1$nparACT_data_min(b, SR, data)
+  if (SR != 1 / 60) {
+    data_min <- nparACT_data_min(b, SR, data)
   } else {
-    data_min <- data$activity
+    data_min <- .subset2(data, "activity")
   }
   ## ------------------------------------------
-  
+
   ## ---- Calculate hourly averages
-  data_hrs <- nparACT_auxfunctions1$nparACT_data_hrs(data, a, m)
+  data_hrs <- nparACT_data_hrs(data, a, m)
   ## -----------------------------------------------------------------------------
   ## -----------------------------------------------------------------------------
-  
+
   ## ---- Plot hourly data
-  if (plot == T) {
-    nparACT_auxfunctions2$nparACT_plot_hourly(data, data_hrs, SR)
+  if (plot == TRUE) {
+    nparACT_plot_hourly(data, data_hrs, SR)
   }
-  
+
   ## ---- IS/IV calculation (based on data_hrs!)
-  result_ISIV <- nparACT_ISIVfunctions$nparACT_ISIV(data_hrs, bin_hr)
+  result_ISIV <- nparACT_ISIV(data_hrs, bin_hr)
   IS <- result_ISIV[1]
   IV <- result_ISIV[2]
   ## ---------------------------------------------------------------------------------
-  
+
   ## ---------- Relative Amplitude (RA) calculation
   ## ---- Minutewise averages across 24hrs
-  minaverage <- nparACT_auxfunctions1$nparACT_minaverage(a, data_min)
+  minaverage <- nparACT_minaverage(a, data_min)
   ## --------------------------------
-  
+
   ## ---- Plot Minutewise averages
-  if (plot == T){
-    start.time = NULL
-    nparACT_auxfunctions2$nparACT_plot_minaverage(data, minaverage, start.time, a, SR)
+  if (plot == TRUE) {
+    nparACT_plot_minaverage(data, minaverage, a, SR)
   }
-  ## --------------------------------      
-  
+  ## --------------------------------
+
   ## ---- Plot Hourly averages
-  if (plot == T){
-    nparACT_auxfunctions2$nparACT_plot_hraverage(data, minaverage, start.time, a, SR)
+  if (plot == TRUE) {
+    nparACT_plot_hraverage(data, minaverage, a, SR)
   }
-  ## --------------------------------      
-  
+  ## --------------------------------
+
   ## ---- L5, M10, Lflex values
-  result_RA <- nparACT_RAfunctions$nparACT_L5M10Lflex(data, minaverage, a, SR, minutes)
+  result_RA <- nparACT_L5M10Lflex(data, minaverage, a, SR, minutes)
   L5 <- result_RA[1]
   L5_starttime <- result_RA[2]
   M10 <- result_RA[3]
@@ -91,5 +127,5 @@ function (name, SR, cutoff = 1, minutes, plot = T, fulldays = T){
   RA <- result_RA[7]
 
   nparACT_result <- data.frame(IS, IV, RA, L5, L5_starttime, M10, M10_starttime, Lflex, Lflex_starttime)
-  return (nparACT_result)
+  return(nparACT_result)
 }
